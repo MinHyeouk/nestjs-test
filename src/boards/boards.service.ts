8fb402d85703f1af8a561f6c3560cc9e53e13f95
93bulldozer <fdmin930905@gmail.com>
2022-02-18 00:45:54 +0900

    boardcreate method를 boradservice에  생성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -6,5 +8,16 @@ export class BoardsService {
 
   getAllBoards() {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid,
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
   }
 }
