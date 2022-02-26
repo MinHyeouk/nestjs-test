37f6939fa5512f319ae458a9c5e4cd86b97959ec
93bulldozer <fdmin930905@gmail.com>
2022-02-26 16:29:29 +0900

    불필요한 코드 삭제

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -21,21 +21,10 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  // @Get('/')
-  // getAllBoard(): Board[] {
-  //   return this.boardsService.getAllBoards();
-  // }
-
   @Get()
   getAllBoard(): Promise<Board[]> {
     return this.boardsService.getAllBoards();
   }
-
-  // @Post()
-  // @UsePipes(ValidationPipe)
-  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-  //   return this.boardsService.createBoard(createBoardDto);
-  // }
 
   @Post()
   @UsePipes(ValidationPipe)
@@ -48,15 +37,10 @@ export class BoardsController {
     return this.boardsService.getBoardById(id);
   }
 
-  // @Get('/:id')
-  // getBoardById(@Param('id') id: string): Board {
-  //   return this.boardsService.getBoardById(id);
-  // }
-
-  // @Delete('/:id')
-  // deleteBoard(@Param('id') id: string): void {
-  //   this.boardsService.deleteBoard(id);
-  // }
+  @Delete('/:id')
+  deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.boardsService.deleteBoard(id);
+  }
 
   @Patch('/:id/status')
   updateBoardStatus(
@@ -65,11 +49,4 @@ export class BoardsController {
   ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
-  // @Patch('/:id/status')
-  // updateBoardStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  // ) {
-  //   return this.boardsService.updateBoardStatus(id, status);
-  // }
 }
